ad9f87be7d5fe5e734ca9990dd81180825bad3e9
divydividivu <64409788+divydividivu@users.noreply.github.com>
2020-09-26 19:19:48 +0530

    Add files via upload
    Added the python file

---
+++ e-RSA.py
@@ -0,0 +1,26 @@
+from Crypto.Util.number import *
+
+def find_invpow(x,n):
+    """Finds the integer component of the n'th root of x,
+    an integer such that y ** n <= x < (y + 1) ** n.
+    """
+    high = 1
+    while high ** n < x:
+        high *= 2
+    low = high//2
+    while low < high:
+        mid = (low + high) // 2
+        if low < mid and mid**n < x:
+            low = mid
+        elif high > mid and mid**n > x:
+            high = mid
+        else:
+            return mid
+    return mid + 1
+
+c = int(input("Enter the value of c: "))
+e=int(input("Enter the value of e: "))
+
+m=find_invpow(c,e)
+
+print(long_to_bytes(m))
